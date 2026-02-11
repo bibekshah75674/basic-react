470a1077d335f69f1fcbadaa57a6eb755c7506eb
Bibek <bibekshah78272@gmail.com>
2026-02-11 16:15:00 +0545

    fixed small error in h3 totalWeeks

--- src/assets/CourseCard.jsx
+++ src/assets/CourseCard.jsx
@@ -5,7 +5,7 @@ export const CourseCard = ({item})=>{
                 <img src={item.image} alt=""/>
                 <h2 className="font-bold bg-gray-300">{item.name}</h2>
                 <p>{item.description}</p>
-                <h3>Total Weeks:{item.totaWeeks}</h3>
+                <h3>Total Weeks:{item.totalWeeks}</h3>
                 <h3 className="font-bold bg-green-100">Lessons:{item.lessons}</h3>
                 <h2 className="font-bold bg-green-100">Instructor:{item.instructor}</h2>
             </div>
