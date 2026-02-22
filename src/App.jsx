ba4d702e81cfb599e66954bc256d1bab46a37c2f
Bibek <bibekshah78272@gmail.com>
2026-02-22 14:04:06 +0545

    added links

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
 import BlogPage from "./assets/BlogPage";
 // import {UserList} from "./assets/UserList";
-// import NewsPortal from "./assets/NewsPortal";
+import NewsPortal from "./assets/NewsPortal";
 
 // import NewTodoList from "./assets/Class/Newtodolist";
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
 import BlogDetailPage from "./BlogDetailPage";
+import NewNav from "./assets/NewNav";
 
 
 function App() {
   return (
     <>
+    <NewNav title={"My Website"}/>
       {/* <NewTodoList/> */}
       {/* <BlogPage/> */}
       {/* <UserList/> */}
-      {/* <NewsPortal/> */}
       <Routes>
-        <Route path="/" element={<BlogPage/>}></Route>
-        <Route path="blogs" element={<BlogDetailPage/>}></Route>
+        <Route path="/" element={<h1>This is the home page</h1>}></Route>
+        <Route path="about" element={<h1>This is about page</h1>}></Route>
+        <Route path="blogs" element={<BlogPage/>}></Route>
+        <Route path="news" element={<NewsPortal/>}></Route>
+        <Route path="contact" element={<h1>This is contact page</h1>}></Route>
       </Routes>
     </>
   );
---
+++ src/assets/NewNav.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function NewNav(props) {
+  return (
+    <>
+    <div className='flex justify-between items-center p-8 bg-orange-500'>
+      <div className='text-4xl font-medium text-amber-50'>
+        <h1>{props.title}</h1>
+      </div>
+      <div>
+        <ul className='flex justify-between items-center gap-4 text-xl'>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="about">About</Link></li>
+          <li><Link to="blogs">Blog</Link></li>
+          <li><Link to="news">News</Link></li>
+          <li><Link to="contact">Contact</Link></li>
+        </ul>
+      </div>
+      <div>
+        <button className='text-lg bg-amber-50 text-orange-500 px-4 py-1 rounded-lg hover:bg-amber-200 transition'>Login</button>
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default NewNav
