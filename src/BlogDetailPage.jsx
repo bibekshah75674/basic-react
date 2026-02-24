35d0de8990706cec96e48e00d34bfbf8ecadfebf
Bibek <bibekshah78272@gmail.com>
2026-02-24 14:51:39 +0545

    added back to blogs button

--- src/BlogDetailPage.jsx
+++ src/BlogDetailPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import { useSearchParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 
 const BlogDetailPage = () => {
@@ -17,12 +18,18 @@ const BlogDetailPage = () => {
     } 
   return (
     <>
+    <div className='m-8'>
     <h1>Blog id:{filteredBlogsById.id}</h1>
     <h1>{filteredBlogsById.title}</h1>
     <h1>{filteredBlogsById.description}</h1>
     <h1>{filteredBlogsById.author}</h1>
     <h1>{filteredBlogsById.date}</h1>
     <img src={filteredBlogsById.image} className='h-96 w-96'  alt="" />
+
+    <Link to="/blogs">
+    <button className='mt-2 bg-orange-600 hover:bg-orange-700 text-white px-4 py-1 rounded-lg transition font-semibold text-base'>Back to Blogs</button>
+    </Link>
+    </div>
     </>
   )
 }
